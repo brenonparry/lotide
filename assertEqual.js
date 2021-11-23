4dea22530e9d13dd616dfcf506b7cc3d73dcce93
Brenon Parry <brenonparry@gmail.com>
2021-11-23 01:24:10 +0000

    using the console.assert

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("These pretzles are making me thirsty", "These pretzles are making me thirsty");
+assertEqual(42, 54);
+assertEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertEqual("Mulva", "Doloris");
