053254f6f7704475391cd3d3bb9c593e64042adb
Brenon Parry <brenonparry@gmail.com>
2021-11-26 03:40:04 +0000

    Created takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,25 @@
+
+const takeUntil = function(array, callback) {
+  let finalArr = [];
+  for (const element of array) {
+    if (callback(element)) {
+      return finalArr;
+    } else {
+      finalArr.push(element)
+    }
+  }
+  return finalArr;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+console.log(results2);
+
