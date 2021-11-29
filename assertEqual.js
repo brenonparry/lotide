93e303fee5f29f0505f3347893a4cd9c89d8bddf
Brenon Parry <brenonparry@gmail.com>
2021-11-29 21:36:17 +0000

    Updated code to work with test files

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("These pretzles are making me thirsty", "These pretzles are making me thirsty");
-assertEqual(42, 54);
-assertEqual([1, 2, 3, 4], [1, 2, 3, 4]);
-assertEqual("Mulva", "Doloris");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("These pretzles are making me thirsty", "These pretzles are making me thirsty");
+// assertEqual(42, 54);
+// assertEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+// assertEqual("Mulva", "Doloris");
+
+module.exports = assertEqual;
