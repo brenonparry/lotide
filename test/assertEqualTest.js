bfcd58fc3f51ffaf5506f625097abb301250d65a
Brenon Parry <brenonparry@gmail.com>
2021-11-29 21:35:30 +0000

    updated assertEqual.js and head.js by adding test files for each

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("These pretzles are making me thirsty", "These pretzles are making me thirsty");
+assertEqual(42, 54);
+assertEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertEqual("Mulva", "Doloris");
