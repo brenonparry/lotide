816bcbcef42ea49eb09cab629e40b657b5037da0
Brenon Parry <brenonparry@gmail.com>
2021-11-29 23:45:01 +0000

    Updated code to work with test file

--- middle.js
+++ middle.js
@@ -1,30 +1,12 @@
-const eqArrays = function(arrX, arrY) {
-  if (arrX.length === arrY.length) {
-    for (let i = 0; i < arrX.length; i++) {
-      if (arrX[i] !== arrY[i]) {
-        return false
-      } 
-    }
-    return true
-  }
-  return false
-}
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo) === true) {
-    console.log(`Would you look at that?! ${arrayOne} === ${arrayTwo}, which can only mean these Arrays are the same!`);
-  } else {
-    console.log(`Shoot dang heck. Looks like ${arrayOne} !== ${arrayTwo}, back to the drawing board`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 
 
-const testArr = [1, 2, 3, 4, 5, 6, 7]
 
 const middle = function(array) {
-  let index = Math.floor((testArr.length / 2))
+  let index = Math.floor((array.length / 2))
   let newArr = [];
   if (array.length > 2) {
     if (array.length % 2 === 0) {
@@ -37,22 +19,7 @@ const middle = function(array) {
   return newArr;
 }
 
-console.log(middle(testArr));
 
 
 
-
-// const index = Math.floor((testArr.length / 2))
-// console.log(testArr[num - 1]);
-// console.log(testArr[num]);
-
-// if (array.length % 2 === 0) {
-// newArr.push(array[num - 1]);
-// newArr.push(array[num]);
-// }
-// else
-
-
-
-// console.log(Math.floor((testArr.length / 2)))
-// console.log(num);
+module.exports = middle;
