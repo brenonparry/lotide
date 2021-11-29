b100d0bc9fc462c8ed9f694f08f7202126401d3e
Brenon Parry <brenonparry@gmail.com>
2021-11-29 03:58:07 +0000

    Updated notes and code

--- eqArrays.js
+++ eqArrays.js
@@ -6,7 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
+// _______________________ My original code didnt account for some conditions so a mentor helped me work it out _______________
 // const eqArrays = function(arrX, arrY) {
 //   if (arrX.toString() === arrY.toString()) {
 //     return true
@@ -16,9 +16,9 @@ const assertEqual = function(actual, expected) {
 // }
 
 const eqArrays = function(arrX, arrY) {
-  if (arrX.length === arrY.length) {
-    for (let i = 0; i < arrX.length; i++) {
-      if (arrX[i] !== arrY[i]) {
+  if (arrX.length === arrY.length) { // compare .lengths
+    for (let i = 0; i < arrX.length; i++) { // loop through on of the arrays
+      if (arrX[i] !== arrY[i]) { // compare to a falsy 
         return false
       } 
     }
