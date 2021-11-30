9036bbb7df10e4ec884217cf59b353f5b2c6f699
Brenon Parry <brenonparry@gmail.com>
2021-11-30 00:37:02 +0000

    Updated tailTest.js to work with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,26 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns 7 for [5, 6, 7]", () => {
+    assert.strictEqual(tail([5, 6, 7]), 7);
+  });
+  it("returns Labs for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+  });
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(tail([1]), 1);
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(tail([]), undefined);
+  });
+});
 
 
 
-assertEqual(tail([1, 2, 3, 4, 5, 6, 7]), 7);
-assertEqual(tail([5, 6, 7]), 7);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
-assertEqual(tail([1, 2, 3]), 3);
-assertEqual(tail([1]), 1);
-assertEqual(tail([]), undefined);
+
+
+
